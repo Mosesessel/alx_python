77cbfa8941c4b483d9364209d9b172c78481009f
MOSES ALEX ESSEL <111113973+Mosesessel@users.noreply.github.com>
2023-07-25 01:34:42 +0100

    ...

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,2 +1,3 @@
-for i in range(90):
-    print("{:02d}".format(i), end=', ' if i < 99 else '\n')
+for i in range(10):
+    for j in range(i+1, 10):
+        print("{:d}{:d}".format(i, j), end=', ' if i < 8 else '\n')
