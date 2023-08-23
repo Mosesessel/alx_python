2c579017bbe70a20110fa993d053e1aa226db7a5
MOSES ALEX ESSEL <111113973+Mosesessel@users.noreply.github.com>
2023-08-23 12:36:16 +0100

    Update 4-fibonacci.py

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,17 +1,15 @@
-#!/usr/bin/python3
 def fibonacci_sequence(n):
-    if n <= 0:
-        return[]
-    elif n == 1:
-        return [0]
-    elif n == 2:    
-        return [0, 1]
-    elif n == 2:
-        return [n]
-    
-    fibonacci_sequence = [0, 1, 1] #  Initialize with first three Fibonacci numbers
-    while len(fibonacci_sequence) < n:
-        next = fibonacci_sequence[-1] + fibonacci_sequence[-2]
-        fibonacci_sequence.append(next)
-        return fibonacci_sequence 
+    fibonacci_list = [0, 1]
+    for i in range (n):
+        if i <= 1:
+            fibonacci_list.append(i)
+        else:
+            fibonacci_next = fibonacci_list[-1] + fibonacci_list[-2] 
+            fibonacci_list.append(fibonacci_next)       
+    return fibonacci_list
 
+
+print(fibonacci_sequence(6))
+print(fibonacci_sequence(1))
+print(fibonacci_sequence(0))
+print(fibonacci_sequence(20))
